86382e3daf15c6660120087aac8d6f4a405ab43d
nikhil-mlk <nikhil.mlk19861986@gmail.com>
2025-07-21 13:49:03 -0700

    added port 4444 in main.tf file

--- main.tf
+++ main.tf
@@ -66,6 +66,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Selenium Grid port"
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
